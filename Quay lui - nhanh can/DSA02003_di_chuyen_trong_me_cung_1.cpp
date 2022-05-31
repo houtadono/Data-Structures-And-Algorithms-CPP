2d06350cddd91ae4dc9e13cee0d99024ed5bc821
Houta <houtadono@gmail.com>
2022-05-31 22:53:14 +0700

    Update DSA02003_di_chuyen_trong_me_cung_1.cpp

--- Quay lui - nhanh can/DSA02003_di_chuyen_trong_me_cung_1.cpp
+++ Quay lui - nhanh can/DSA02003_di_chuyen_trong_me_cung_1.cpp
@@ -1,52 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define For(i,a,b) for(int i=a;i<b;i++)
-int a[11][11],n,kt;
 vector<string> res;
-void in()
-{
-    For(i,0,res.size())
-        cout<<res[i]<<' ';
-    res.clear();
+int n,a[11][11];
+void ql(int i,int j, string s){
+	if(!a[0][0]) return;
+	if(i==n-1&&j==i) res.push_back(s);
+	if(i+1<n&&a[i+1][j]) ql(i+1,j,s+"D");
+	if(j+1<n&&a[i][j+1]) ql(i,j+1,s+"R");
 }
 
-void ttsinh(int i,int j,string s1)
-{
-    if(!a[0][0])
-    {
-        return ;
-    }
-    if(i==j&&i==n-1&&a[i][j])
-    {
-        res.push_back(s1);
-        kt=1;
-        return ;
-    }
-    if(a[i+1][j]&&i<n-1) 
-        ttsinh(i+1,j,s1+"D");
-
-    if(a[i][j+1]&&j<n-1) 
-        ttsinh(i,j+1,s1+"R");
-}
-
-void solve()
-{
-    kt=0;
-    cin>>n;
-    For(i,0,n)
-        For(j,0,n) 
-            cin>>a[i][j];
-    ttsinh(0,0,"");
-    if(!kt) cout<<-1;
-    else    in();
-    cout<<endl;
-}
-
-int main()
-{
-    int t;cin>>t;
-    while(t--)
-    {
-        solve();
-    }
+int main(){
+    int t; cin>>t;
+    while(t--){
+    	cin>>n;
+    	for(int i=0;i<n;i++)
+    		for(int j=0;j<n;j++)
+    			cin>>a[i][j];
+    	ql(0,0,"");
+    	if(res.empty()) cout<<-1;
+    	else
+    		for(int i=0;i<res.size();i++) cout<<res[i]<<' ';
+    	cout<<endl;
+    	res.clear();
+	}
+	return 0;
 }
